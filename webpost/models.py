e1fb8b5aec74a0f2618eee4226793d16f16507fb
kukanji <111495470+kukanji@users.noreply.github.com>
2022-12-20 12:49:12 +0900

    モデルを追加しました

--- webpost/models.py
+++ webpost/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 EVALUATION_CHOICES = [('良い', '良い'), ('悪い', '悪い')]
+
 class ReviewModel(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
@@ -11,4 +12,18 @@ class ReviewModel(models.Model):
     images = models.ImageField(upload_to='')
     useful_review = models.IntegerField(null = True, blank = True, default = 0)
     useful_review_record = models.TextField()
-    evaluation = models.CharField(max_length=10, choices = EVALUATION_CHOICES)
+    evaluation = models.CharField(max_length=10, choices = EVALUATION_CHOICES) 
+
+
+class ImageModel(models.Model):
+    image = models.ImageField(upload_to='')
+    category_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CategoryModel(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    description1 = models.TextField()
+    description2 = models.TextField()
+    laugage = models.IntegerField(default=1)
