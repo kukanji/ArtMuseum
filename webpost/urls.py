20377c7b896efc4af0cc7eea1b25f795b176402e
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-22 14:08:55 +0900

    top, user, images_listview,imageviewの追加とパスの追加

--- webpost/urls.py
+++ webpost/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from .views import signupview, loginview, logoutview, listview, detailview, CreateClass, evaluationview
+from .views import signupview, loginview, logoutview, imageview, images_listview,CreateImage, CreateCategory, top, user
 
 urlpatterns = [
     # 管理画面
-    path('signup',  signupview, name = 'signup'),
-    path('login', loginview, name = 'login'),
-    path('logout', logoutview, name = 'logout'),
+    path('signup/',  signupview, name = 'signup'),
+    path('login/', loginview, name = 'login'),
+    path('logout/', logoutview, name = 'logout'),
 
     # 非管理画面
-    path('<str:username>/images/list', listview, name = 'list'),
-    path('images/<int:pk>', detailview, name = 'detail'),
-    path('images/create', CreateClass.as_view(), name = 'create'),
-    path('evaluation/<int:pk>', evaluationview, name = 'evaluation')
+    path('/', top, name = 'top'),
+    path('<str:username>/', user, name = 'user'),
+    path('<str:username>/<int:category_name/>', images_listview, name = 'images_list'),
+    path('<int:category_name>/image/<int:pk>', imageview, name = 'image'),
+    path('image/create_image/', CreateImage.as_view(), name = 'create_image'),
+    path('<str:username>/create_category', CreateCategory.as_view(), name = 'create_cateroy'),
+    # path('evaluation/<int:pk>', evaluationview, name = 'evaluation')
 ]
--- webpost/views/images.py
+++ webpost/views/images.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
-from ..models import ReviewModel
+from ..models import ImageModel, CategoryModel
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -10,24 +10,31 @@ def listview(request, username):
     # ユーザーを取得
     user = User.objects.filter(username=username)
     # そのユーザーの画像を取得
-    object_list = ReviewModel.objects.filter(author=user[0])
+    object_list = CategoryModel.objects.filter(author=user[0])
     return render(request, 'list.html', {'object_list': object_list})
 
 
-def detailview(request, pk):
-    object = ReviewModel.objects.get(pk = pk)
-    return render(request, 'detail.html', {'object': object})
+def imagesview(request, pk):
+    object = ImageModel.objects.get(pk = pk)
+    username = object.author.username
+    return render(request, 'images.html', {'object': object, 'username': username})
 
-
-class CreateClass(CreateView):
+# 新しいカテゴリーを作成した後
+class CreateCategory(CreateView): 
     template_name = 'create.html'
-    model = ReviewModel
-    fields = ('title', 'content', 'author', 'images', 'evaluation')
-    success_url = reverse_lazy('list')
+    model = CategoryModel
+    fields = ('name', 'author_id', 'description1', 'description2', 'language')
+    success_url = reverse_lazy('users.html')
+
+class CreateImage(CreateView):
+    template_name = 'create.html'
+    model = ImageModel
+    fields = ('image', 'category_id', 'author_id')
+    success_url = reverse_lazy('images_list')
 
 
 
-def evaluationview(request, pk):
+"""def evaluationview(request, pk):
     post = ReviewModel.objects.get(pk = pk)
     author_name = request.user.get_username() + str(request.user.id)
     if author_name in post.useful_review_record:
@@ -37,3 +44,4 @@ def evaluationview(request, pk):
         post.useful_review_record = post.useful_review_record + author_name
         post.save()
         return redirect('list')
+"""
