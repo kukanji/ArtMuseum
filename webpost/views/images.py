8cb3c77761068a1ab9aae9f0c9cdfa0576e99ed5
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-22 14:10:29 +0900

    imageファイルの更新

--- webpost/views/images.py
+++ webpost/views/images.py
@@ -6,28 +6,40 @@ from ..models import ImageModel, CategoryModel
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-def listview(request, username):
+# トップページ
+def top(request):
+    return render(request, 'top.html')
+
+# ユーザーごとのホームページ（カテゴリー名も表示しておく）
+def user(request, username):
+    # ユーザーのカテゴリーデータをuser.htmlに送らなくてはならない
+    user = User.objects.filter(username=username)
+    category_list = CategoryModel.objects.filter(author_id = user[0])
+    return render(request, 'user.html', {'object_list': category_list})
+
+# 写真のリスト表示
+def images_listview(request, username):
     # ユーザーを取得
     user = User.objects.filter(username=username)
-    # そのユーザーの画像を取得
-    object_list = CategoryModel.objects.filter(author=user[0])
-    return render(request, 'list.html', {'object_list': object_list})
+    # そのユーザーの画像を取得、カテゴリーモデルから写真のリストを取得する
+    image_list = ImageModel.objects.filter(author_id=user[0])
+    return render(request, 'images_list.html', {'object_list': image_list})
 
-
-def imagesview(request, pk):
+# 写真リスト内の写真をタップすると写真を拡大する
+def imageview(request, pk):
     object = ImageModel.objects.get(pk = pk)
-    username = object.author.username
-    return render(request, 'images.html', {'object': object, 'username': username})
+    #username = object.author.username
+    return render(request, 'image.html', {'object': object})
 
 # 新しいカテゴリーを作成した後
 class CreateCategory(CreateView): 
-    template_name = 'create.html'
+    template_name = 'create_category.html'
     model = CategoryModel
     fields = ('name', 'author_id', 'description1', 'description2', 'language')
-    success_url = reverse_lazy('users.html')
+    success_url = reverse_lazy('user.html')
 
 class CreateImage(CreateView):
-    template_name = 'create.html'
+    template_name = 'create_image.html'
     model = ImageModel
     fields = ('image', 'category_id', 'author_id')
     success_url = reverse_lazy('images_list')
