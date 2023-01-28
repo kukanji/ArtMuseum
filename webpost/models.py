f6ab4a2f235215537c8c55e2c95c8f40d4147d6d
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-28 15:00:36 +0900

    ImageModelのcategory_idをManyToManyFieldに変更

--- webpost/models.py
+++ webpost/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 EVALUATION_CHOICES = [('良い', '良い'), ('悪い', '悪い')]
 
-class ReviewModel(models.Model):
+"""class ReviewModel(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -13,7 +13,7 @@ class ReviewModel(models.Model):
     useful_review = models.IntegerField(null = True, blank = True, default = 0)
     useful_review_record = models.TextField()
     evaluation = models.CharField(max_length=10, choices = EVALUATION_CHOICES) 
-
+"""
 class CategoryModel(models.Model):
     name = models.CharField(max_length=100)
     author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_category')
@@ -24,5 +24,5 @@ class CategoryModel(models.Model):
 
 class ImageModel(models.Model):
     image = models.ImageField(upload_to='')
-    category_id = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    category_id = models.ManyToManyField(CategoryModel)
     author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_image')
