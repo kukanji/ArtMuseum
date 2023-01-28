ad22822f814cca23a29a41e37bdc0c6f6b9ec05b
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-28 14:59:11 +0900

    被管理画面のパスの整理

--- webpost/urls.py
+++ webpost/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import signupview, loginview, logoutview, imageview, images_listview,CreateImage, CreateCategory, top, user
+from .views import imageview, images_listview, top, user
 
 urlpatterns = [
     # 管理画面
-    path('signup/',  signupview, name = 'signup'),
-    path('login/', loginview, name = 'login'),
-    path('logout/', logoutview, name = 'logout'),
+    #path('signup/',  signupview, name = 'signup'),
+    #path('login/', loginview, name = 'login'),
+    #path('logout/', logoutview, name = 'logout'),
 
     # 非管理画面
-    path('/', top, name = 'top'),
+    path('', top, name = 'top'),
     path('<str:username>/', user, name = 'user'),
-    path('<str:username>/<int:category_name/>', images_listview, name = 'images_list'),
-    path('<int:category_name>/image/<int:pk>', imageview, name = 'image'),
-    path('image/create_image/', CreateImage.as_view(), name = 'create_image'),
-    path('<str:username>/create_category', CreateCategory.as_view(), name = 'create_cateroy'),
-    # path('evaluation/<int:pk>', evaluationview, name = 'evaluation')
+    path('<str:username>/<str:category_name>/', images_listview, name = 'images_list'),
+    path('<str:category_name>/image/<int:pk>', imageview, name = 'image'),
+    #path('image/create_image/', CreateImage.as_view(), name = 'create_image'),
+    #path('<str:username>/create_category', CreateCategory.as_view(), name = 'create_cateroy'),
+    #path('evaluation/<int:pk>', evaluationview, name = 'evaluation')
 ]
