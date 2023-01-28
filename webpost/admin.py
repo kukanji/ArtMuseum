9535af586f06d380d78af8811267f7e6027914c6
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-28 15:09:18 +0900

    adminへのモデルの追加

--- webpost/admin.py
+++ webpost/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import ReviewModel
+#from .models import ReviewModel
+from.models import CategoryModel, ImageModel
 # Register your models here.
 
-admin.site.register(ReviewModel)
+#admin.site.register(ReviewModel)
+admin.site.register(CategoryModel)
+admin.site.register(ImageModel)
