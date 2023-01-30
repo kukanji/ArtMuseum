af05e83e8d699c727fc37e7e4ddde1bb54d3d9c0
kukanji <111495470+kukanji@users.noreply.github.com>
2023-01-30 22:10:40 +0900

    user, images_listのビューファイルを変更

--- webpost/views/images.py
+++ webpost/views/images.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
-from ..models import ImageModel, CategoryModel
+from ..models import Image, Category, UserHome
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -14,35 +14,44 @@ def top(request):
 # ユーザーごとのホームページ（カテゴリー名も表示しておく）
 def user(request, username):
     # ユーザーのカテゴリーデータをuser.htmlに送らなくてはならない
-    user = User.objects.filter(username=username)
-    category_list = CategoryModel.objects.filter(author_id = user[0])
-    return render(request, 'user.html', {'category_list': category_list})
+    user = User.objects.get(username=username)
+    category_list = Category.objects.filter(author = user)
+    user_home = UserHome.objects.get(author = user)
+    return render(request, 'user.html', {'category_list': category_list, 
+                                         'user_home': user_home,
+                                         'username': username})
 
 # 写真のリスト表示
-def images_listview(request, username, lcategory_list):
-    # ユーザーを取得
-    user = User.objects.filter(username=username)
+def images_list(request, username, category_name):
+    category = Category.objects.get(name = category_name)
     # そのユーザーの画像を取得、カテゴリーモデルから写真のリストを取得する
-    image_list = ImageModel.objects.filter(author_id=user[0], )
-    category_list = CategoryModel.objects.filter(author_id = user[0])
-    return render(request, 'images_list.html', {'object_list': image_list, 'category_list': category_list})
+    image_list = Image.objects.filter(category = category)
+    # ユーザーを取得
+    user = User.objects.get(username=username)
+    #category_list = Category.objects.filter(author = user)
+    category = Category.objects.get(name = category_name)
+    return render(request, 'images_list.html', {
+        'image_list': image_list,
+        'category': category,
+        'username': username,
+        })
 
 # 写真リスト内の写真をタップすると写真を拡大する
-def imageview(request, pk):
-    object = ImageModel.objects.get(pk = pk)
-    #username = object.author.username
-    return render(request, 'image.html', {'object': object})
+#def imageview(request, pk):
+#    object = Image.objects.get(pk = pk)
+#    #username = object.author.username
+#    return render(request, 'image.html', {'object': object})
 
 # 新しいカテゴリーを作成した後
 #class CreateCategory(CreateView): 
 #    template_name = 'create_category.html'
-#    model = CategoryModel
+#    model = Category
 #    fields = ('name', 'author_id', 'description1', 'description2', 'language')
 #    success_url = reverse_lazy('user.html')
 #
 #class CreateImage(CreateView):
 #    template_name = 'create_image.html'
-#    model = ImageModel
+#    model = Image
 #    fields = ('image', 'category_id', 'author_id')
 #    success_url = reverse_lazy('images_list')
 
