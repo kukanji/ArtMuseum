cf81e89d9d47a5b28c1b6a848b648ae00f2a9000
kukanji <111495470+kukanji@users.noreply.github.com>
2023-02-19 20:34:47 +0900

    add static_root

--- webproject/settings.py
+++ webproject/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = env('STATIC_ROOT')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
